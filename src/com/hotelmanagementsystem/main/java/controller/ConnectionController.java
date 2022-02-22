161b1c667d6802783e461b9b7376324e9a2ba8a4
urosr <urosrelicc@gmail.com>
2022-02-22 10:43:30 +0100

    update connection parameters

--- src/com/hotelmanagementsystem/main/java/controller/ConnectionController.java
+++ src/com/hotelmanagementsystem/main/java/controller/ConnectionController.java
@@ -8,8 +8,8 @@ public class ConnectionController {
     public static Connection getConnection() {
         try {
             // If you are using XAMPP, the connection is usually localhost:3306
-            return DriverManager.getConnection("jdbc:mysql://yourconnection/hotel",
-                    "your username", "your password");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel",
+                    "root", "");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             return null;
